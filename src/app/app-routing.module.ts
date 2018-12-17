13200846524b1f088a5c2cdde88234c5a684e257
Leannelb <leannelaceybyrne@outlook.com>
2018-12-17 21:59:04 +0100

    routerlink working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { BooksComponent } from './components/books/books.component';
 
 const routes: Routes = [];
 
