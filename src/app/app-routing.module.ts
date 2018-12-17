b289e192bccf114d56e4eab39bd27120cedb8dbf
Leannelb <leannelaceybyrne@outlook.com>
2018-12-17 21:42:26 +0100

    navbar and additional sections added - not fully working yet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BooksComponent } from './components/books/books.component';
 
 const routes: Routes = [];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,26 +6,23 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { CodeSnippitsComponent } from './components/code-snippits/code-snippits.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { ResumeComponent } from './components/resume/resume.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
-
+import { BooksComponent } from './components/books/books.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 const appRoutes: Routes = [
-  { 
-    path: 'about', 
-    component: HomeComponent 
-  },
-  {
-    path: 'portfolio',
-    component: PortfolioComponent
-  }
-
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'about', component: AboutMeComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'code', component: CodeSnippitsComponent },
+  { path: 'books', component: BooksComponent }
 ];
-
 
 @NgModule({
   declarations: [
@@ -36,14 +33,20 @@ const appRoutes: Routes = [
     PortfolioComponent,
     CodeSnippitsComponent,
     AboutMeComponent,
-    FooterComponent
+    FooterComponent,
+    BooksComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes), // <-- installs Router routes, components and services
     AppRoutingModule,
     AngularFontAwesomeModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_BASE_HREF, useValue: window['_app_base'] || '/'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
