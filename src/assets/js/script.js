9b6b817eaeb5fff5e4831ba2dc8839a8b0d2e0a5
Elaine <elainevellac@gmail.com>
2019-02-23 16:42:00 +0100

    Install easing and smooth scroll for nav

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,3 +1,44 @@
+(function($) {
+	$(document).ready(function() {
+
+		//Smooth Scroll
+		var scrollAnimationTime = 1200,
+	        scrollAnimation = 'easeInOutExpo';
+	    $('a.scrollto').bind('click.smoothscroll', function (event) {
+	        var target = this.hash;
+	        if($(target).length){
+		        event.preventDefault();
+		        $('html, body').stop().animate({
+		            'scrollTop': $(target).offset().top
+		        }, scrollAnimationTime, scrollAnimation, function () {
+		            /*window.location.hash = target;*/
+		        });
+	        }
+	    });
+
+	});
+
+	$(window).resize(function () {
+	    waitForFinalEvent(function(){
+
+	    }, 500, "TE7c79NYwz");
+	});
+
+	var waitForFinalEvent = (function () {
+	  var timers = {};
+	  return function (callback, ms, uniqueId) {
+	    if (!uniqueId) {
+	      uniqueId = "Don't call this twice without a uniqueId";
+	    }
+	    if (timers[uniqueId]) {
+	      clearTimeout (timers[uniqueId]);
+	    }
+	    timers[uniqueId] = setTimeout(callback, ms);
+	  };
+	})();
+
+})(jQuery);
+
 
 function scroll_to(clicked_link, nav_height) {
 	var element_class = clicked_link.attr('href').replace('#', '.');
@@ -11,12 +52,10 @@ function scroll_to(clicked_link, nav_height) {
 	}
 }
 
-
+/*
 jQuery(document).ready(function() {
-	
-	/*
-	    Navigation
-	*/
+
+	//Navigation
 	$('a.scroll-link').on('click', function(e) {
 		e.preventDefault();
 		scroll_to($(this), $('nav').outerHeight());
@@ -25,16 +64,12 @@ jQuery(document).ready(function() {
 	$('.top-content .text').waypoint(function() {
 		$('nav').toggleClass('navbar-no-bg');
 	});
-	
-    /*
-        Background slideshow
-    */
+
+    //Background slideshow
     $('.top-content').backstretch("assets/img/backgrounds/1.jpg");
     $('.section-4-container').backstretch("assets/img/backgrounds/1.jpg");
-    
-    /*
-        Wow
-    */
+
+    //Wow
     new WOW().init();
-	
-});
+
+});*/
