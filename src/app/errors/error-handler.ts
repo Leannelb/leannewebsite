9e1dc9a88b0f8597202182310f1c8e28816a4bf3
leannelb <leannelaceybyrne@outlook.co>
2020-03-08 20:19:19 +0000

    regigged nav bar to old buttons, html blog to be implemented

---
+++ src/app/errors/error-handler.ts
@@ -0,0 +1,18 @@
+import { ErrorHandler, Injectable } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+
+
+@Injectable()
+
+export class GlobalErrorHandler implements ErrorHandler {
+    handleError(error: any) {
+        if (error instanceof HttpErrorResponse) {
+            //Backend returns unsuccessful response codes such as 404, 500 etc.				  
+            console.error('Backend returned status code: ', error.status);
+            console.error('Response body:', error.message);          	  
+        } else {
+            //A client-side or network error occurred.	          
+            console.error('An error occurred:', error.message);          
+        }     
+}
+}
