048892348465e40d6f741a9560b9526afae5d936
leannelb <leannelaceybyrne@outlook.co>
2020-11-14 20:36:37 +0000

    in a great plac blog cms working

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,21 @@
+// export const environment = {
+//   production: true,
+//   firebase: {
+//     apiKey: "AIzaSyDtENvfqQBalG8rL-8Rh_bMvQYVCuXJyNw",
+//     authDomain: "lillycode-6e8fe.firebaseapp.com",
+//     databaseURL: "https://lillycode-6e8fe.firebaseio.com",
+//     projectId: "lillycode-6e8fe",
+//     storageBucket: "lillycode-6e8fe.appspot.com",
+//     messagingSenderId: "685727283630",
+//     appId: "1:685727283630:web:61dfc801eb9c7310e3303f",
+//     measurementId: "G-6RD00FXVH3"
+//   }
+// };
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyDtENvfqQBalG8rL-8Rh_bMvQYVCuXJyNw",
-    authDomain: "lillycode-6e8fe.firebaseapp.com",
-    databaseURL: "https://lillycode-6e8fe.firebaseio.com",
-    projectId: "lillycode-6e8fe",
-    storageBucket: "lillycode-6e8fe.appspot.com",
-    messagingSenderId: "685727283630",
-    appId: "1:685727283630:web:61dfc801eb9c7310e3303f",
-    measurementId: "G-6RD00FXVH3"
+  production: false,
+
+  contentful: {
+    spaceId: 'dw7mwkq576rx',
+    token: 'Idi9wPHEbBl7zahJDcCntqMfDrMjHUB06P2PELbEOSM'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,20 +2,6 @@
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-// export const environment = {
-//   production: false,
-//   firebase: {
-//     apiKey: "AIzaSyDtENvfqQBalG8rL-8Rh_bMvQYVCuXJyNw",
-//     authDomain: "lillycode-6e8fe.firebaseapp.com",
-//     databaseURL: "https://lillycode-6e8fe.firebaseio.com",
-//     projectId: "lillycode-6e8fe",
-//     storageBucket: "lillycode-6e8fe.appspot.com",
-//     messagingSenderId: "685727283630",
-//     appId: "1:685727283630:web:61dfc801eb9c7310e3303f",
-//     measurementId: "G-6RD00FXVH3"
-//   },
-// };
-
 export const environment = {
   production: false,
 
